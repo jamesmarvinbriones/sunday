2fa479ae2bd31b041d6bf06cb36cdc6a23600862
James <jamesmarvinbriones@gmail.com>
2019-05-21 05:47:04 +0800

    1

---
+++ js/google-sheets-html.js
@@ -0,0 +1,26 @@
+
+
+google.load('visualization', '1', {
+    packages: ['table']
+});
+var visualization;
+
+function drawVisualization() {
+    var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1yrrLJcpsueMT-tbZn21u1ybiVqHARcUVxpy2eecnZ5o&output=html&usp=sharing');
+    query.setQuery('SELECT A, B, C, D label A "Current Date", B "Last Sunday", C "Day", D "x"');
+    query.send(handleQueryResponse);
+}
+
+function handleQueryResponse(response) {
+    if (response.isError()) {
+        alert('There was a problem with your query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+    var data = response.getDataTable();
+    visualization = new google.visualization.Table(document.getElementById('table'));
+    visualization.draw(data, {
+        allowHtml: true,
+        legend: 'bottom'
+    });
+}
+google.setOnLoadCallback(drawVisualization);
